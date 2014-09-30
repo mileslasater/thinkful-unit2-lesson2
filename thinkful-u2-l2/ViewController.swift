11bdb568eb6ac370c5ea185710f82605ee9a41ac
Miles Lasater <miles@mileslasater.com>
2014-09-30 15:49:02 -0400

    adding iOS single view project initial files

---
+++ thinkful-u2-l2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thinkful-u2-l2
+//
+//  Created by Miles Lasater on 9/30/14.
+//  Copyright (c) 2014 Miles Lasater. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ thinkful-u2-l2Tests/thinkful_u2_l2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  thinkful_u2_l2Tests.swift
+//  thinkful-u2-l2Tests
+//
+//  Created by Miles Lasater on 9/30/14.
+//  Copyright (c) 2014 Miles Lasater. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class thinkful_u2_l2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
